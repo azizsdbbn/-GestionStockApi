57afec8f541afe0de6385e53963d206d43e640b7
Abdou@sdbbn <94757744+sdbbn@users.noreply.github.com>
2023-01-23 23:36:38 +0000

    first commit

---
+++ src/test/java/sn/sdbbn/gestionstockapi/GestionStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.sdbbn.gestionstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
